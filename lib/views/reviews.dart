be5df2ac63687151b13c5e192362207875a1d312
xtomko06 <xtomko06@stud.fit.vutbr.cz>
2023-12-15 22:21:34 +0100

    Add create reviews

--- lib/views/reviews.dart
+++ lib/views/reviews.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../controllers/ReviewsController.dart';
 import '../models/Review.dart';
 
@@ -16,6 +17,8 @@ class _ReviewsPageState extends State<ReviewsPage> {
   final ReviewsController _reviewsController = ReviewsController();
   late Future<List<Review>> reviewsFuture;
   double averageRating = 0.0;
+  final TextEditingController reviewController = TextEditingController();
+  int userRating = 0;
 
   @override
   void initState() {
@@ -41,6 +44,60 @@ class _ReviewsPageState extends State<ReviewsPage> {
       if (mounted) {
         setState(() {});
       }
+    }
+  }
+
+  Future<void> _addReview() async {
+    if (reviewController.text.isEmpty || userRating == 0) {
+      // Display an error message if review or rating is not provided
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Please provide a review and rating.'),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return;
+    }
+
+    try {
+      Review newReview = Review(
+        id: '', // Will be assigned by Firebase
+        event: widget.eventId,
+        review: reviewController.text,
+        rating: userRating,
+      );
+
+      // Save the new review to Firebase
+      await FirebaseFirestore.instance.collection('reviews').add({
+        'event': newReview.event,
+        'review': newReview.review,
+        'rating': newReview.rating,
+      });
+
+      // Clear the text field and reset the user rating
+      reviewController.clear();
+      userRating = 0;
+
+      // Refresh the reviews and update the average rating
+      reviewsFuture = _reviewsController.fetchReviews(widget.eventId);
+      _calculateAverageRating();
+
+      // Display a success message
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Review added successfully!'),
+          backgroundColor: Colors.green,
+        ),
+      );
+    } catch (e) {
+      print('Error adding review: $e');
+      // Display an error message
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error adding review. Please try again.'),
+          backgroundColor: Colors.red,
+        ),
+      );
     }
   }
 
@@ -101,6 +158,65 @@ class _ReviewsPageState extends State<ReviewsPage> {
             ),
           ),
           SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Add Your Review',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+                TextField(
+                  controller: reviewController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your review...',
+                    fillColor: Colors.black,
+                    filled: true,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Your Rating',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+                Row(
+                  children: List.generate(
+                    5,
+                    (index) => GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          userRating = index + 1;
+                        });
+                      },
+                      child: Icon(
+                        Icons.star,
+                        color: index < userRating ? Colors.amber : Colors.grey,
+                        size: 40,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: _addReview,
+                  child: Text('Submit Review',
+                      style: TextStyle(color: Colors.white)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
           Expanded(
             child: FutureBuilder<List<Review>>(
               future: reviewsFuture,
@@ -127,8 +243,11 @@ class _ReviewsPageState extends State<ReviewsPage> {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              'Review: ${review.review}',
-                              style: TextStyle(color: Colors.white),
+                              '${review.review}',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 17,
+                              ),
                             ),
                             Row(
                               children: List.generate(
